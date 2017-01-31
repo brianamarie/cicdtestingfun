07ee83b0bd1a1b14945cc0d820d980f410f9ae13
Briana Swift <brianamarie@github.com>
2017-01-31 15:45:14 -0600

    start file for different colors test

---
+++ tests/test_differentcolors.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class TestDifferentColors < Test::Unit::TestCase
+
+  def test_different_colors
+    #open file and read
+    # search for color pieces, add each color to an array
+    # compare all pieces of the array
+    # exit(1) if any of them are equal
+    assert(min_range, "The start speed is not between 0 and 1.")
+  end
+
+end
