b51d47985c5efc61450290d38b1244e99eda6d6e
Briana Swift <brianamarie@github.com>
2017-01-31 13:26:35 -0600

    replacing ruby test with one expected to fail

--- tests/test_speedupgame.rb
+++ tests/test_speedupgame.rb
@@ -1,18 +1,34 @@
 require 'test/unit'
-require "yaml"
 
-class TestCoordinateValidation < Test::Unit::TestCase
+class TestDirectoryContents < Test::Unit::TestCase
 
-  def test_coordinatevalidation
-    Dir["./_pins/*.json"].each do |path|
-      f = YAML.load_file(path)
-      [
-        f["latitude"],
-        f["longitude"],
-      ].each do |s|
-        assert_not_nil(s.to_s[/^-?\d+(?:\.\d+)?$/], path)
-      end
+  def test_no_other_files_root
+    correct_files = [".bundle",
+     ".git",
+     ".github",
+     ".gitignore",
+     ".travis.yml",
+     ".yamllint",
+     "Gemfile",
+     "Gemfile.lock",
+     "README.md",
+     "_config.yml",
+     "_layouts",
+     "_pins",
+     "createMap.topojson",
+     "index.html",
+     "readme.md",
+     "render.js",
+     "tests",
+     "vendor"]
+    correct_files_sorted = correct_files.sort
+    actual_files = []
+    puts correct_files_sorted
+    Dir.foreach('./') do |item|
+      next if item == '.' or item == '..'
+      actual_files.push(item)
     end
-
+    actual_files_sorted = actual_files.sort
+    assert_equal(correct_files_sorted, actual_files_sorted)
   end
 end
