1aee37c1343afd275a14801cf09422fb463118d9
Briana Swift <brianamarie@github.com>
2017-01-31 13:54:09 -0600

    remove the falsey gem

--- tests/test_speedupgame.rb
+++ tests/test_speedupgame.rb
@@ -6,7 +6,6 @@ class TestGameSpeed < Test::Unit::TestCase
 # make sure they are within a given range
 
   def test_game_speed
-    SearchInFile.search( "./index.html", "decrement" )
     assert_equal(true, true)
   end
 
